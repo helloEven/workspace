f3620d70ed1e94ce625320a3165e1e84c28b49a2
刘杰 <liujie@liujiedeMacBook-Air.local>
2016-06-08 14:46:17 +0800

    测试一个视图控制器

---
+++ LJFree/LJFree/LJView.h
@@ -0,0 +1,13 @@
+//
+//  LJView.h
+//  LJFree
+//
+//  Created by 刘杰 on 16/6/8.
+//  Copyright © 2016年 even. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJView : UIView
+
+@end
---
+++ LJFree/LJFree/LJView.m
@@ -0,0 +1,21 @@
+//
+//  LJView.m
+//  LJFree
+//
+//  Created by 刘杰 on 16/6/8.
+//  Copyright © 2016年 even. All rights reserved.
+//
+
+#import "LJView.h"
+
+@implementation LJView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
