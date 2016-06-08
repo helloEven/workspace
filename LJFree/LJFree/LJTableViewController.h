1c3e0406dd6bd98dbcdd54d55bb134e4b54af6f4
刘杰 <liujie@liujiedemacbook-air.local>
2016-06-08 15:21:28 +0800

    加视图控制器

---
+++ LJFree/LJFree/LJTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LJTableViewController.h
+//  LJFree
+//
+//  Created by 刘杰 on 16/6/8.
+//  Copyright © 2016年 even. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJTableViewController : UITableViewController
+
+@end
